2650f417ec916dcf899b847cdadd1573fb5297dd
jessegraham <jesse@fluentinnovations.com>
2020-12-16 23:54:26 -0500

    filter commented post.

--- main.py
+++ main.py
@@ -7,26 +7,25 @@ import os
 
 # Login info please enter your client ID, Client Secret obtained here https://old.reddit.com/prefs/apps/
 # You will also need to enter your username and password. User_agent is the name you want to the bot to display
-bot = praw.Reddit(user_agent="Naughty Marie", client_id="I0i4OOSupvEkpg", client_secret="G6fQ8ss9rTz6fIf3UlCFDKpo8_0",
-                  username="naughtymarie", password="chester1985")
-
+#Change this info
+token = 'enter your client_id here'
+secret = 'client secret goes here'
+UserName = 'enter your username'
+Password = 'replace this with your password'
+# Do not change this
+bot = praw.Reddit(user_agent=UserName, client_id=token, client_secret=secret,
+                  username=UserName, password=Password)
 bot.validate_on_submit = True
 
 
 # Enter your comment phrases here surrounded by parentheses, separated by a comma ie, "wow", "that's awesome!"
-phrases = ['damn that is hot!', 'sexy!', 'mmm yummy!', '🥵🥵🥵', 'can I join you?', 'I love seeing your post!❤️❤️❤️',
-           'damn 💦💦💦', '😻😻😻', '💦💦💦', '😍😍😍😍', ]
+phrases = ['I love seeing your post!❤️❤️❤️', 'damn 💦💦💦', '😻😻😻', '💦💦💦', '😍😍😍😍', ]
 tp = len(phrases) - 1
 
 # Enter subreddit names with the next number followed by a colon and contained in apostrophes
 subs = {1: 'fitgirls', 2: 'BreedingMaterial', 3: 'Nsfw_Amateurs', 4: 'TheThiccness', 5: 'SlimThick', 6: 'curvy',
         7: 'assinthong', 8: 'AmazingCurves', 9: 'whooties', 10: 'booty_queens', 11: 'thickwhitegirls', 12: 'RateMyAss',
-        13: 'knockmeup', 14: 'naturaltitties', 15: 'DadWouldBeProud', 16: 'BonerMaterial', 17: 'TotalBabes', 18: 'nsfw2',
-        19: 'DirtyConfession', 20: 'interracialwild', 21: 'TexasCuckoldCommunity', 22: 'HotwivesCuckold',
-        23: 'happycuckold', 24: 'Cuckold', 25: 'curvy', 26: 'couplesgonewild', 27: 'BlackIsBetter', 28: 'BBCSluts',
-        29: 'SnowBunnys', 30: 'amateurgirlsbigcocks', 31: 'homemadexxx', 32: 'Nsfw_Amateurs', 33: 'slutwife',
-        34: 'DadWouldBeProud', 35: 'NaughtyWives', 36: 'HotStuffNSFW', 37: 'AmateurSlutWives', 38: 'homesex',
-        39: 'nsfw2', 40: 'SnowBunnys', 41: 'RedGIFsPorn', 42: 'DirtyConfession'}
+        }
 ts = len(subs) - 1
 
 def sleeptimer(time):
@@ -73,7 +72,7 @@ def comment():
                 f.write(str(new_post) + "\n")
             f.close()
 
-        except Exception as e:
+        except BaseException as e:
             print(e)
 
     else:
